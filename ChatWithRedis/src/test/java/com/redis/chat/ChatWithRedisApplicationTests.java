7d336438287e135b567f4da13abc6c33ac6aea0e
jadussi <jadussi788@gmail.com>
2023-06-13 20:54:51 +0900

    스프링부트 2.7.12 자바 11 그래들 7.6

---
+++ ChatWithRedis/src/test/java/com/redis/chat/ChatWithRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWithRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

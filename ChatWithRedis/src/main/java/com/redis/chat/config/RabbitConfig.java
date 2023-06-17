1207da32e78f41711468ed4b2c46d5a6c9320b38
jadussi <jadussi788@gmail.com>
2023-06-17 13:57:00 +0900

    1. 개인톡 rabbitmq binding시 routing key 와일드 카드 #에서 * 로 변경 2. 단체톡 rabbitmq 설정 추가

--- ChatWithRedis/src/main/java/com/redis/chat/config/RabbitConfig.java
+++ ChatWithRedis/src/main/java/com/redis/chat/config/RabbitConfig.java
@@ -4,6 +4,7 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -20,7 +21,7 @@ public class RabbitConfig {
 	// 개인톡 관련 전역변수 선언
 	private static final String PRIVATE_CHAT_QUEUE = "privateChat.queue";
 	private static final String PRIVATE_CHAT_EXCHAGE = "privateChat.exchage";
-	private static final String PRIVATE_CHAT_KEY = "privateChat.key.#";
+	private static final String PRIVATE_CHAT_KEY = "privateChat.key.*";	// * 하나의 단어만 대체하는 와일드카드, # 여러개의 단어를 대체하는 와일드카드
 	
 	@Bean	// 개인톡 큐 생성
 	Queue privateChatQueue() {
@@ -32,20 +33,40 @@ public class RabbitConfig {
 		return new DirectExchange(PRIVATE_CHAT_EXCHAGE);
 	}
 	
-	@Bean	// 개인톡 privateChat.key.# 라우팅 키를 사용하여 큐, exchange 바인딩 설정
+	@Bean	// 개인톡 privateChat.key.* 라우팅 키를 사용하여 큐, exchange 바인딩 설정
 	Binding privateChatBinding(@Qualifier("privateChatQueue") Queue queue, @Qualifier("privateChatExchange") DirectExchange exchange) {
 		return BindingBuilder.bind(queue).to(exchange).with(PRIVATE_CHAT_KEY);
 	}
 	
+	// 단체톡 관련 전역번수 선언
+	private static final String PUBLIC_CHAT_QUEUE = "publicChat.queue";
+	private static final String PUBLIC_CHAT_EXCHAGE = "publicChat.exchage";
+	private static final String PUBLIC_CHAT_KEY = "puvlicChat.key.*";
+	
+	@Bean	// 단체톡 큐 생성
+	Queue publicChatQueue() {
+		return new Queue(PUBLIC_CHAT_QUEUE);
+	}
+	
+	@Bean	// 단체톡 topic 방식 exchange 생성
+	TopicExchange publicChatExchange() {
+		return new TopicExchange(PUBLIC_CHAT_EXCHAGE);
+	}
+	
+	@Bean	// 단체톡 publicChat.key.* 라우팅 키를 사용하여 큐, exchange 바인딩 설정
+	Binding publicChatBinding(@Qualifier("publicChatQueue") Queue queue, @Qualifier("publicChatExchange") TopicExchange topicExchange) {
+		return BindingBuilder.bind(queue).to(topicExchange).with(PUBLIC_CHAT_KEY);
+	}
+	
 	@Bean
-    public MessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public MessageConverter messageConverter() {	
+        return new Jackson2JsonMessageConverter();	// json형식의 messageConverter 설정
     }
 	
 	@Bean
 	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
 		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-		rabbitTemplate.setMessageConverter(messageConverter());	//object에 담을 message rabbitmq의 메세지 형식으로 변환(json)
+		rabbitTemplate.setMessageConverter(messageConverter());	//object에 담을 message rabbitmq의 메세지 형식으로 변환
 		return rabbitTemplate;
 	}
 }

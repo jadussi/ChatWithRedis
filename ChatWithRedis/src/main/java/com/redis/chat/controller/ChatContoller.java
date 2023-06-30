60b023b788c99801dc245dba420685640b578455
jadussi <jadussi788@gmail.com>
2023-06-30 23:22:31 +0900

    RequestBody 어노테이션 변경 Swagger UI test 시 파라메터 입력 시 QueryString에서 json으로 입력

--- ChatWithRedis/src/main/java/com/redis/chat/controller/ChatContoller.java
+++ ChatWithRedis/src/main/java/com/redis/chat/controller/ChatContoller.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,6 @@ import com.redis.chat.service.ChatService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 
 @RestController

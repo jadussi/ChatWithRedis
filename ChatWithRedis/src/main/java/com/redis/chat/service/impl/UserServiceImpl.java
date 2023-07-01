d26b2be5e6736a3acadbfd71f55ff7ae33d823a1
jadussi <jadussi788@gmail.com>
2023-07-02 02:35:53 +0900

    사용하지 않는 소스 삭제(import문)

--- ChatWithRedis/src/main/java/com/redis/chat/service/impl/UserServiceImpl.java
+++ ChatWithRedis/src/main/java/com/redis/chat/service/impl/UserServiceImpl.java
@@ -1,7 +1,5 @@
 package com.redis.chat.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.redis.chat.dao.UserDAO;
